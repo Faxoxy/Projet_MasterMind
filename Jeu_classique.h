4ba2f4ab6c199a7b463de4bb2572b83d40178147
V. <162312453+Faxoxy@users.noreply.github.com>
2025-11-07 16:59:16 +0000

    ...

---
+++ Jeu_classique.h
@@ -0,0 +1,10 @@
+#ifndef JEU_CLASSIQUE_H
+#define JEU_CLASSIQUE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void lancer_jeu_classique(void);
+int verifier_combinaison(const int proposition[], const int combinaison_secrete[]);
+
+#endif
